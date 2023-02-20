7ac19c8c373d824e13be7a1bacc33e24c1bb0f45
MonoKnight <brboldt@ilstu.edu>
2023-02-20 17:38:55 -0600

    02/20/23
    gabe if you read this  you smell lolllllll

---
+++ Assets/Assets/Scripts/FadetoBlack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class FadetoBlack : MonoBehaviour
+{
+    [SerializeField] GameObject Black;
+    [SerializeField] GameObject Player;
+    private Image black2;
+    [SerializeField] int deathY;
+    [SerializeField] int deathPlaneDistance;
+    private float playery;
+    private byte imageAlpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        black2 = Black.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playery = Player.GetComponent<Transform>().position.y;
+        if (playery > (deathY + deathPlaneDistance)) black2.color = new Color32(0, 0, 0, 0);
+        else if (playery <= (deathY + deathPlaneDistance) && playery >= deathY)
+        {
+            imageAlpha = (byte)(((deathPlaneDistance - playery + deathY) / deathPlaneDistance) * 256);
+            black2.color = new Color32(255, 255, 255, imageAlpha);
+        }
+        else if (playery < deathY)
+        {
+            black2.color = new Color32(255, 255, 255, 255);
+        }
+    }
+}
